b27102ad90b8f9e985fa9f88ee2fb449961a4f01
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-19 12:11:34 -0400

    display date Objects & not use context

--- pages/user/[uuid].js
+++ pages/user/[uuid].js
@@ -1,13 +1,13 @@
-// access usersList context
-import { useContext } from 'react';
-import UserListContext from '../contexts/userList';
-// Next.js routing - access URL props
-import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
+
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
+// custom components
+import NotesList from '../../components/NotesList';
 
-const UserNotFound = (uuid) => (
+const UserNotFound = ({ uuid }) => (
   <div>
     <Head>
       <title>User Not Found | Installer Site</title>
@@ -24,13 +24,11 @@ const UserNotFound = (uuid) => (
   </div>
 )
 
-export default function UserPage() {
-  const [userList, setUserList] = useContext(UserListContext);
+export default function UserPage({ userList, setUserList }) {
+  // access route parameters (uuid is a string )
   const router = useRouter()
   const { uuid } = router.query
-
   
-
   const user = userList.find(o => o.id === uuid)
   // check if user exists
   if (!user) {
@@ -49,6 +47,17 @@ export default function UserPage() {
     })
     setUserList(newUserList)
   }
+  console.log(user)
+  console.log(typeof user.dateLastTalked)
+  // REST API turns dates into strings - I parse string and create date from dateString
+  const dateLastTalked = new Date(JSON.parse(user.dateLastTalked))
+  const dateCreated = new Date(JSON.parse(user.dateLastTalked))
+  console.log(dateLastTalked)
+  console.log(dateCreated)
+  // console.log(user.dateLastTalked.toLocaleDateString('us-US', {  year: 'numeric', month: 'short', day: 'numeric' }))
+  useEffect(() => {
+    console.log(userList)
+  }, [userList])
 
   return (
     <div>
@@ -61,10 +70,11 @@ export default function UserPage() {
         <h3>Short Bio</h3>
         <p>{user.bio}</p>
         <h3>Date Last Talked</h3>
-        <p>{user.dateLastTalked.toLocaleDateString('us-US', {  year: 'numeric', month: 'short', day: 'numeric' })}</p>
+        <p>{dateLastTalked.toLocaleDateString('us-US', {  year: 'numeric', month: 'short', day: 'numeric' })}</p>
         <h5>Met Through: {user.metThrough}</h5>
+        <h5>Date Created: {dateCreated.toLocaleDateString('us-US', {  year: 'numeric', month: 'short', day: 'numeric' })}</h5>
         <h3>Notes</h3>
-        <NotesList notesList={user.notesList} updateUserNotes={updateUserNotes} />
+        <NotesList notesList={user.notesList} setNotesList={updateUserNotes} />        
         <h3>Online Accounts</h3>
       </main>
     </div>
