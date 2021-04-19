80f4dd19e315792759de701bb24a71f6a2b46704
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-19 11:16:53 -0400

    hold state for all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import { useState } from 'react'
 import '../styles/globals.css'
-import DefaultLayout from '../components/_layouts/DefaultLayout';
+import DefaultLayout from '../components/_layouts/DefaultLayout'
 
 function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout || (page => <DefaultLayout children={page} />)
-  return getLayout(<Component {...pageProps} />)
+  const [userList, setUserList] = useState([]);
+  const getLayout = Component.getLayout || (page => <DefaultLayout children={page} userList={userList} setUserList={setUserList} />)
+
+  return getLayout(
+    <Component {...pageProps} userList={userList} setUserList={setUserList} />
+  )
 }
 
 export default MyApp
