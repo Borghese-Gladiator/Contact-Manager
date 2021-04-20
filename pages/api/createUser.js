16d5bf753c46e7407dde736b99791c8d862d7849
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-20 04:48:55 -0400

    add numerous data members

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -3,22 +3,32 @@ export default (req, res) => {
   if (req.method === 'POST') {
     // Process a POST request
     const parsedBody = JSON.parse(req.body)
-    const { name, bio, metThrough } = parsedBody
+    const { name } = parsedBody
     const { v4: uuidv4 } = require('uuid');
-  
+
     res.status(200).json({
       id: uuidv4(),
       name: name,
-      bio: bio,
-      metThrough: metThrough,
+      bioObject: {
+        age: "25",
+        location: "Belmont, MA, USA",
+        gender: "Male",
+        languages: ["Cantonese", "Mandarin"],
+        occupation: "Student (Master's)",
+        major: "CS (Reinforcement Learning)"
+      },
+      friendGroup: 'League of Legends',
       notesList: [
         { id: uuidv4(), text: "Wash dishes", done: false },
         { id: uuidv4(), text: "Do laundry", done: false },
         { id: uuidv4(), text: "Take shower", done: false }
       ],
-      onlineAccountsObj: {},
+      onlineAccountsList: [
+        { id: uuidv4(), text: "Discord - Blah#1111" },
+        { id: uuidv4(), text: "Messenger - Blah" },
+      ],
       dateLastTalked: JSON.stringify(new Date()),
-      dateCreated: JSON.stringify(new Date())
+      dateMet: JSON.stringify(new Date())
     })
   } else {
     res.status(400).json({
