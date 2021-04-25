684d30247bbbbee59a8cf7e447d9aed77179d693
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-25 10:37:58 -0400

    MOVE ContactList & NoteList into 1 folder & update paths

--- pages/user/[uuid].js
+++ pages/user/[uuid].js
@@ -4,8 +4,8 @@ import Head from 'next/head'
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
 // custom components
-import NoteList from '../../components/NoteList';
-import ContactList from '../../components/ContactList';
+import NoteList from '../../components/ListComponents/NoteList';
+import ContactList from '../../components/ListComponents/ContactList';
 import InlineEdit from '../../components/InlineEdit';
 import Row from '../../components/Flexbox/Row';
 import Col from '../../components/Flexbox/Col';
