8f801f61a3203c6a49a3e9696c04fb38adbe6570
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-11-30 01:13:47 -0500

    Remove AbsoluteMenu from DefaultLayout

--- src/components/_layouts/DefaultLayout.jsx
+++ src/components/_layouts/DefaultLayout.jsx
@@ -1,11 +1,4 @@
-// Routing w/ Next.js
-import Link from 'next/link';
 import Sidebar from './Sidebar';
-import AbsoluteMenu from '../AbsoluteMenu';
-// icons
-import { TiDelete } from 'react-icons/ti';
-import { IoMdPersonAdd } from 'react-icons/io';
-// styling
 import styles from './DefaultLayout.module.css';
 
 const DefaultLayout = ({ userList=[], setUserList, children }) => (
@@ -13,17 +6,6 @@ const DefaultLayout = ({ userList=[], setUserList, children }) => (
     <nav className={styles.sidebar}>
       <Sidebar userList={userList} setUserList={setUserList} />
     </nav>
-    <AbsoluteMenu position="top_right">
-      <Link href="/user/create">
-        <a className={styles.vertical_align_text}><IoMdPersonAdd style={{ fontSize: '30' }} />Create Page</a>
-      </Link>
-      <Link href="/user/delete">
-        <a className={styles.vertical_align_text}><TiDelete style={{ fontSize: '30' }} />Delete Page</a>
-      </Link>
-      <Link href="/user/mass_create">
-        <a className={styles.vertical_align_text}><IoMdPersonAdd style={{ fontSize: '30' }} />Mass Create</a>
-      </Link>
-    </AbsoluteMenu>
     <div style={{ flexGrow: 1 }}>
       {children}
     </div>
