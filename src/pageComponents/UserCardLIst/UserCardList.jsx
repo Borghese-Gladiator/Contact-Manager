fe6a594690980c9ce114a44ad170df3042e4a607
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-12-14 00:25:45 -0500

    UserCardList add div styling

--- src/pageComponents/UserCardLIst/UserCardList.jsx
+++ src/pageComponents/UserCardLIst/UserCardList.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import styles from './UserCardList.module.css';
 // custom
 import AbsoluteBtn from '../../components/AbsoluteBtn';
-import Card from '../../components/Card';
 // icons
 import { AiFillDelete } from 'react-icons/ai';
 import { BsPlusCircle } from 'react-icons/bs';
@@ -15,24 +14,24 @@ const dateTodayObj = new Date();
 const AddUserCard = () => {
   const addUser = () => console.log("ADD USER MODAL");
   return (
-    <Card className={styles.add_btn} onClick={addUser}>
-      <BsPlusCircle />
-    </Card>
+    <button className={styles.add_btn} onClick={addUser}>
+      <BsPlusCircle style={{fontSize: 25}} />
+    </button>
   )
 }
 
 function UserCardList({ userList, deleteUserList }) {
-  console.log(userList);
-  const [checkedState, setCheckedState] = useState(
-    new Array(userList.length).fill(false)
-  );
-  console.log(checkedState);
+  const initialCheckedState = new Array(userList.length).fill(false)
+  console.log(initialCheckedState)
+  const [checkedState, setCheckedState] = useState(initialCheckedState);
   const handleOnChange = (position) => {
+    console.log("CALLED ON CHANGE")
+    console.log(position)
     const updatedCheckedState = checkedState.map((item, index) =>
       index === position ? !item : item
     );
+    console.log(updatedCheckedState)
     setCheckedState(updatedCheckedState);
-    console.log(checkedState)
   };
 
   let noCheckedItems = !checkedState.includes(true);
@@ -55,34 +54,36 @@ function UserCardList({ userList, deleteUserList }) {
             <AiFillDelete style={{ fontSize: 20 }} />
           </AbsoluteBtn>
       }
-      <div>
-      {
-        userList.map(({ id, name, shortBio, dateLastTalked, placeLastTalked, contactMethod }, idx) => {
-          // Parse string date into date object
-          const dateLastTalkedObj = new Date(JSON.parse(dateLastTalked));
-          const daysSinceLastTalk = dateDifference(dateTodayObj, dateLastTalkedObj);
-          const daysText = daysSinceLastTalk === 1 ? "day" : "days";
-          const truncatedText = truncateDescription(shortBio);
-          return (
-            <Card key={`user_card_${idx}`} onClick={() => handleOnChange(idx)}>
-              <input
-                type="checkbox"
-                checked={checkedState[idx]}
-                onChange={() => handleOnChange(idx)}
-              />
-              <h2>{name}</h2>
-              <h4>Last talked: {daysSinceLastTalk} {daysText} ago</h4>
-              <h4>Last talked at: {placeLastTalked}</h4>
-              <span>Contact by: {contactMethod}</span>
-              <span style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                {truncatedText}
-              </span>
-            </Card>
-          )
-        })
-      }
+      <div className={styles.card_list}>
+        {
+          userList.map(({ id, name, shortBio, dateLastTalked, placeLastTalked, contactMethod }, idx) => {
+            // Parse string date into date object
+            const dateLastTalkedObj = new Date(JSON.parse(dateLastTalked));
+            const daysSinceLastTalk = dateDifference(dateTodayObj, dateLastTalkedObj);
+            const daysText = daysSinceLastTalk === 1 ? "day" : "days";
+            const truncatedText = truncateDescription(shortBio);
+            console.log(`${checkedState}`)
+            console.log(`CHECKED STATE: ${checkedState[idx]}`)
+            return (
+              <div className={`${styles.card} ${checkedState[idx] ? styles.active_card : ""}`} key={`user_card_${idx}`} onClick={() => handleOnChange(idx)}>
+                <input
+                  type="checkbox"
+                  checked={checkedState[idx]}
+                  onChange={() => handleOnChange(idx)}
+                />
+                <h2>{name}</h2>
+                <h4>Last talked: {daysSinceLastTalk} {daysText} ago</h4>
+                <h4>Last talked at: {placeLastTalked}</h4>
+                <span>Contact by: {contactMethod}</span>
+                <span style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                  {truncatedText}
+                </span>
+              </div>
+            )
+          })
+        }
+        <AddUserCard />
       </div>
-      <AddUserCard />
     </>
   );
 };
