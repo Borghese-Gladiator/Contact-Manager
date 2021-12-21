98e71d92d1d947e2bb82995841c790582e992fe1
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-12-20 22:35:16 -0500

    Add sort buttons

--- src/pageComponents/UserList/UserList.jsx
+++ src/pageComponents/UserList/UserList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 // Next.js Routing
 import Link from 'next/link';
 // Ant Design components
-import { Row, Col, Typography, Card, Space } from 'antd';
+import { Row, Col, Button, Typography, Card, Space } from 'antd';
 const { Text } = Typography;
 // Custom Components
 import AddUserModal from "./AddUserModal";
@@ -22,7 +22,7 @@ function UserList({ userList, setUserList }) {
     setUserList([...userList, newUser])
   }
   const updateUserKey = (userId, key, newValue) => {
-    setUserList(userList.map((tempUser, idx) => 
+    setUserList(userList.map((tempUser, idx) =>
       // searches list for matching ID && if found, sets key to newValue
       userId === tempUser.id ? { ...tempUser, [key]: newValue } : tempUser
     ))
@@ -34,6 +34,16 @@ function UserList({ userList, setUserList }) {
     setUserList(userList.filter((user) => !idList.includes(user.id)))
   }
   // Handle clicks
+  const handleDateSortBtnClick = () => {
+    const sortedUserList = [].concat(userList)
+      .sort((a, b) => new Date(a.dateLastTalked) > new Date(b.dateLastTalked) ? 1 : -1)
+    setUserList(sortedUserList)
+  }
+  const handleNameSortBtnClick = () => {
+    const sortedUserList = [].concat(userList)
+      .sort((a, b) => a.name.localeCompare(b.name))
+    setUserList(sortedUserList)
+  }
   const handleCardClick = (id) => {
     if (selectedUserIds.includes(id)) {
       // remove if present
@@ -48,7 +58,11 @@ function UserList({ userList, setUserList }) {
     deleteUserList(selectedUserIds)
   }
   return (
-    <>
+    <Space direction="vertical">
+      <Row justify="center">
+        <Button onClick={handleDateSortBtnClick}>Sort by Date Last Talked</Button>
+        <Button onClick={handleNameSortBtnClick}>Sort by Name</Button>
+      </Row>
       <Row gutter={[8, 8]}>
         {
           userList.map(({ id, name, dateLastTalked, placeLastTalked, contactMethod, notesList }, idx) => {
@@ -60,10 +74,10 @@ function UserList({ userList, setUserList }) {
                   size="small"
                   title={name}
                   bordered={false}
-                  extra={<a href={`/user/${id}`}><ExportOutlined style={{fontSize:20}} /></a>}
+                  extra={<a href={`/user/${id}`}><ExportOutlined style={{ fontSize: 20 }} /></a>}
                   className={`${selectedUserIds.includes(id) ? styles.active_card : ""}`}
                 >
-                  <Space direction="vertical">
+                  <Space direction="vertical" size={0}>
                     <Text>{dateText}</Text>
                     <Text>{placeText}</Text>
                   </Space>
@@ -73,17 +87,17 @@ function UserList({ userList, setUserList }) {
             )
           })
         }
-        <Col key={`add-user-btn`} md={3} style={{display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Col key={`add-user-btn`} md={3} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
           <AddUserModal createUser={createUser} />
         </Col>
       </Row>
       {selectedUserIds.length === 0
-      ? <></>
-      : <AbsoluteBtn position="bottom_right" onClick={handleDeleteBtnClick}>
+        ? <></>
+        : <AbsoluteBtn position="bottom_right" onClick={handleDeleteBtnClick}>
           <DeleteOutlined style={{ fontSize: 20 }} />
         </AbsoluteBtn>
       }
-    </>
+    </Space>
   )
 }
 
