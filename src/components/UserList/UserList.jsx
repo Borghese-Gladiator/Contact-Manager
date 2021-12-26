95dcaf12bc54ade5a32412dc5521db6c306a4aaa
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-12-25 21:49:28 -0500

    Add responsive columns

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -75,7 +75,7 @@ function UserList({ userList, setUserList }) {
             const dateText = getMomentText(dateLastTalked);
             const placeText = `${placeLastTalked}`
             return (
-              <Col key={`user-card-${idx}`} md={6} className={`${styles.my_card}`}>
+              <Col key={`user-card-${idx}`} sm={12} md={8} lg={6} xl={4}>
                 <Checkbox onChange={() => handleCheckboxClick(id)} className={styles.my_checkbox} />
                 <div>
                   <Card
