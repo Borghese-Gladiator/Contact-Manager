eae261280e6fe9e258bdaacc16c93e76b1178d17
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-12-30 01:59:27 -0500

    Fix to display text for 0 appointments

--- pages/appointments.js
+++ pages/appointments.js
@@ -5,16 +5,16 @@ const { Title, Text } = Typography;
 import { getMetaWithTitle } from "../src/utils/utils";
 
 export default function AppointmentsPage({ userList = [], setUserList }) {
+  const apptList = userList.filter(({ appointment }) => appointment !== null)
   return (
     <div>
       {getMetaWithTitle("View All Appointments | Contact Manager")}
       <main>
         <Title>Appointment List</Title>
         <Space direction="vertical" style={{width: "100%"}}>
+          {apptList.length <= 0 && <Text>No Appointments</Text>}
           {
-            userList
-              .filter(({ appointment }) => appointment !== null)
-              .map(({ appointment }, idx) => {
+            apptList.map(({ appointment }, idx) => {
                 return (
                   <Card key={`user-appointment-card-${idx}`} hoverable>
                     <Space direction="vertical" style={{ cursor: 'pointer' }}>
