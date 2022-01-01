69f19e7faf5e2cb06dc53484940f3be61b39a9d6
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-12-31 21:23:21 -0500

    Fix import error (removed that stylesheet)

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,13 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router';
 import { Typography, Row, Button, Space } from "antd"
 const { Title, Text } = Typography;
-
-import styles from '../styles/UserListPage.module.css'
 import { getMetaWithTitle } from "../src/utils/utils";
 
 export default function HomePage({ userList = [], setUserList }) {
   return (
     <div>
       {getMetaWithTitle("Home | Contact Manager")}
-      <main className={styles.main}>
+      <main>
         <Title style={{ textAlign: "center" }}>Contact Manager</Title>
         <Text style={{ textAlign: "center" }}>Personal CRM - Quick utility to track people I talked to and how long ago it was.</Text>
         <Row justify="center">
